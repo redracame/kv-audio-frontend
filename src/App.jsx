fdcd778dabb483ef0b2032581b18e477314b2b7d
redracame <linukaauchithya@gmail.com>
2025-02-07 17:45:05 +0530

    practice  alignment  using flex🦌

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,11 @@ function App(){
   return (
     
     < div> 
-    <div  className='w-[800px] h-[800px] bg-yellow-200 relative'>
-    <div className='w-[180px] h-[350px] bg-blue-200'>
+    <div  className='w-[800px] h-[800px] bg-yellow-200 '>
+    <div className='w-[500px] h-[500px] bg-blue-200 flex flex-col  ' >
     <div className='w-[50px] h-[50px] bg-red-500'></div>
-    <div className='w-[50px] h-[50px] bg-green-500 absolute right-[10px] '></div>
-    <div className='w-[50px] h-[50px] bg-yellow-500 fixed right-[5px]'></div>
+    <div className='w-[50px] h-[50px] bg-green-500  '></div>
+    <div className='w-[50px] h-[50px] bg-yellow-500'></div>
     </div>
     </div>
 
