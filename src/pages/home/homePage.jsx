aaa5d1ca33cdd52d2f9449761f1685a56abe5375
redracame <linukaauchithya@gmail.com>
2025-02-10 13:53:04 +0530

    add homePage to gallery,contact,items, and also desing header with create header file.😏

--- src/pages/home/homePage.jsx
+++ src/pages/home/homePage.jsx
@@ -1,8 +1,20 @@
+import { Route, Routes } from "react-router-dom";
+import Header from "../../components/header";
+
 export default function HomePage(){
    return(
-     <div className="w-full h-screen flex justify-center">
- 
-         <Header/>
-     </div>
+     <>
+    <Header/>
+    <div  className="h-[calc(100vh-100px)] w-full">
+        <Routes path="/*"> 
+        <Route path="/contact" element={<h1>Contact</h1>}/>
+        <Route path="/gallery" element={<h1>Gllery</h1>}/>
+        <Route path="/items" element={<h1>Items</h1>}/>
+        <Route path="/" element={<h1>Home</h1>}/>
+        
+        </Routes>
+    </div>
+   </>
+    
    )
 }
