f098cc84027fd1c27d237c93188230514bf17edf
redracame <linukaauchithya@gmail.com>
2025-02-10 12:02:28 +0530

    create  header file in componts folder,then componts newly create componts header add homePage.jsx file 🦅

---
+++ src/components/header.jsx
@@ -0,0 +1,9 @@
+export default function Header(){
+return(
+    <header className="w-full h-[100px] shadow-xl">
+  </header>
+
+)
+
+
+}
--- src/pages/home/homePage.jsx
+++ src/pages/home/homePage.jsx
@@ -1,6 +1,8 @@
 export default function HomePage(){
    return(
-
-
+     <div className="w-full h-screen flex justify-center">
+ 
+         <Header/>
+     </div>
    )
 }
