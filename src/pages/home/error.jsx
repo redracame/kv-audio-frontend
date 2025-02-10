359cbbf057f0d34e279ec361db6d7907201404b0
redracame <linukaauchithya@gmail.com>
2025-02-10 14:34:48 +0530

    add "Go back to home" part  in error file 👍

--- src/pages/home/error.jsx
+++ src/pages/home/error.jsx
@@ -1,7 +1,10 @@
+import { Link } from "react-router-dom";
+
 export  default function ErrorNotFound(){
 return(
     <div>
         <h1>404 Error:Page Not Found</h1>
+        <Link className="bg-[#f0ad37] p-1" to="/">Go back to Home</Link>
     </div>
 
 )
