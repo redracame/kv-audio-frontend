7de559a157de8ac7a1e2306f4d7d4c00a97cb992
redracame <linukaauchithya@gmail.com>
2025-02-11 14:30:20 +0530

    create login folder and loginPage.jsx🤺

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import AdminPage from './pages/admin/adminPage';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import HomePage from './pages/home/homePage';
 import Testing from './components/testing';
+import LoginPage from './pages/login/login';
 function App(){
   
 
@@ -10,6 +11,7 @@ function App(){
     <BrowserRouter>
       <Routes path="/*">
         <Route path="/testing" element={<Testing/>}/>
+        <Route path="/login" element={<LoginPage/>}/>
         <Route path='/admin/*' element={<AdminPage/>}/>
         <Route path='/*' element={<HomePage/>}/>
       
---
+++ src/pages/login/login.jsx
@@ -0,0 +1,9 @@
+import { useState } from "react"
+
+export  default function LoginPage(){
+  const [email,setEmail] =useState("");
+  return <div>
+    <h1>Login</h1>
+   </div>
+
+}
