83d274b66b0f7a79503f5669f9e0467ffe861be5
redracame <linukaauchithya@gmail.com>
2025-02-11 12:38:20 +0530

    add some Padding to error page🤫

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -5,8 +5,8 @@ return(
     <header  className="w-full h-[100px] shadow-xl flex justify-center relative">
     <img src="/logo.png" alt="logo" className="w-[100px] h-[100px] object-cover border-[3px] absolute left-1 rounded-full" />
     <Link to="/" className=" text-[25px] font-bold m-1">Home</Link>
-    <Link to="/contact" className="text-[25px] font-bold m-1">contact</Link>
-    <Link to="/gallery" className="text-[25px] font-bold m-1">gallery</Link>
+    <Link to="/contact" className="text-[25px] font-bold m-1">Contact</Link>
+    <Link to="/gallery" className="text-[25px] font-bold m-1">Gallery</Link>
     <Link to="/items" className="text-[25px] font-bold m-1">Items</Link>
   </header>
 
