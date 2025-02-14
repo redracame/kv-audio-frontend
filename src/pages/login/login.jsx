cca80321db6751a3600de1ebe244a8ab6ecb62ef
redracame <linukaauchithya@gmail.com>
2025-02-14 23:30:17 +0530

    add to loginPage form tag and api refresh wenna eka nawathanna e.preventDefault eka use kara✌️

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,25 +1,46 @@
 import "./login.css";
-import { useState } from "react"
+import { useState } from "react";
 
-export  default function LoginPage(){
-  const [email,setEmail] =useState("");
-  const [password,setPassword] =useState("");
-  
-  function login(){
-     email , password
+export default function LoginPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-  }
+  function handleOneSubmit(e){
+    e.preventDefault()
+    console.log(email, password)
+   }
 
-  return(
-   <div className=" bg-picture w-full h-screen flex justify-center items-center">
-    <div className="w-[400px] h-[400px] backdrop-blur-xl rounded-2xl flex justify-center items-center flex-col relative">
-      <img src="/logo.png" alt="logo" className=" w-[100px] h-[100px] object-cover"/>
-      <input type="email" placeholder="Email" className="w-[300px] h-[30px] mt-6 bg-transparent border-b-2 border-white text-white text-xl outline-none"/>
-      <input type="password" placeholder="Password" className="w-[300px] h-[30px] mt-6 bg-transparent border-b-2 border-white text-white text-xl outline-none"/>
-      <button className="my-8 w-[300px] h-[50px] bg-[#efac38] text-2xl text-white rounded-lg"onclick={login}>Login</button>
+  return (
+    <div className="bg-picture w-full h-screen flex justify-center items-center">
 
-   </div>
-   </div>
-);
+      <form onSubmit={handleOneSubmit}>
+      <div className="w-[400px] h-[400px] backdrop-blur-xl rounded-2xl flex justify-center items-center flex-col relative">
+        <img src="/logo.png" alt="logo" className="w-[100px] h-[100px] object-cover" />
+        
+        <input 
+          type="email" 
+          placeholder="Email" 
+          className="w-[300px] h-[30px] mt-6 bg-transparent border-b-2 border-white text-white text-xl outline-none"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
 
+        <input 
+          type="password" 
+          placeholder="Password" 
+          className="w-[300px] h-[30px] mt-6 bg-transparent border-b-2 border-white text-white text-xl outline-none"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        {/* Fixed onClick event */}
+        <button 
+          className="my-8 w-[300px] h-[50px] bg-[#efac38] text-2xl text-white rounded-lg"
+        > Login
+        </button>
+      </div>
+      </form>
+    </div>
+  );
 }
+
