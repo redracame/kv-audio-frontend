2930e6a65979eaba4b2c46a8927994102f8babd3
redracame <linukaauchithya@gmail.com>
2025-02-14 12:49:19 +0530

    add  picture to login page✌️

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,9 +1,14 @@
+import "./login.css";
 import { useState } from "react"
 
 export  default function LoginPage(){
   const [email,setEmail] =useState("");
-  return <div>
-    <h1>Login</h1>
+  const [password,setPassword] =useState("");
+  return(
+   <div className=" bg-picture w-full h-screen flex justify-center items-center">
+    <div className="w-[400px] h-[400px] backdrop-blur-xl rounded-2xl ">
    </div>
+   </div>
+);
 
 }
