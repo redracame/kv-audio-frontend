264a5fbffd870bd468c5fb3b71e329759a26bf74
redracame <linukaauchithya@gmail.com>
2025-02-18 18:35:21 +0530

    add hover "CiCirclePlus icon "🤺

--- src/pages/admin/adminItem.jsx
+++ src/pages/admin/adminItem.jsx
@@ -1,8 +1,10 @@
+import { CiCirclePlus } from "react-icons/ci";
 export default function AdminItemsPage(){
 
     return(
         <div
-            className= "w-full h-full bg-red-900">
+            className= "w-full h-full relative">
+             <CiCirclePlus className="text-[100px] absolute right-2 bottom-2 hover:text-red-900"/>   
             </div>
 
     )
