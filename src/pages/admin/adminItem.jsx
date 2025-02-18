d3d8c75bd71b104b85431cfa533bb74bc75ab43d
redracame <linukaauchithya@gmail.com>
2025-02-18 18:42:33 +0530

    add cursor-pointer to pluc mark✌️

--- src/pages/admin/adminItem.jsx
+++ src/pages/admin/adminItem.jsx
@@ -4,7 +4,7 @@ export default function AdminItemsPage(){
     return(
         <div
             className= "w-full h-full relative">
-             <CiCirclePlus className="text-[100px] absolute right-2 bottom-2 hover:text-red-900"/>   
+             <CiCirclePlus className="text-[100px] absolute right-2 bottom-2 hover:text-red-900 cursor-pointer"/>   
             </div>
 
     )
