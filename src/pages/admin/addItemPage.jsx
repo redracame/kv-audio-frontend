6255a2cef398ccdade479a511eab5078de3d985d
redracame <linukaauchithya@gmail.com>
2025-02-19 12:12:57 +0530

    add some inputs to "AddItemPage"🤫

--- src/pages/admin/addItemPage.jsx
+++ src/pages/admin/addItemPage.jsx
@@ -1,6 +1,20 @@
 export default function AddItemPage(){
        return(
-        <div className ="w-full h-full  bg-black">
+        <div className ="w-full h-full flex flex-col items-center">
+              <h1> Add items</h1>
+              <div className="w-[400px] border flex flex-col items-center">
+                     <input type="text" placeholder="Product key"/>     
+                     <input type="text" placeholder="Product Name"/> 
+                     <input type="text" placeholder="Product Price"/> 
+                     <select>
+                        <option key="audio">Audio</option>
+                        <option key="lights">Lights</option>
+                        </select>
+                        <input type="text" placeholder="Product Dimensions"/> 
+                        <input type="text" placeholder="Product Description"/> 
+                        
+                     <button>Add</button>
+         </div>
        </div>
        )
 
