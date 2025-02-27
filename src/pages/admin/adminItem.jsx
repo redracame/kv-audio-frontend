a7c5fa7a1e23ed594db94b3ff40ab19ae849ac9d
redracame <linukaauchithya@gmail.com>
2025-02-27 07:50:36 +0530

    create table in adminItem page✌️

--- src/pages/admin/adminItem.jsx
+++ src/pages/admin/adminItem.jsx
@@ -51,14 +51,43 @@ const sampleArr = [
     }
   ];
   
+import { useState } from "react";
 import { CiCirclePlus } from "react-icons/ci";
 import { Link } from "react-router-dom";
 export default function AdminItemsPage(){
+  const[items,setItems]= useState(sampleArr)
 
     return(
         <div
-            className= "w-full h-full relative
-            ">
+            className= "w-full h-full relative">
+              <table>
+                  <thead>
+                     <th>Key</th>
+                     <th>Name</th>
+                     <th>Price</th>
+                     <th>Category</th>
+                     <th>Dimensions</th>
+                     <th>Availability</th>
+                     </thead> 
+                     <tbody>
+                     {
+                        items.map((product)=>{
+                           console.log(product)
+                           return (
+                             <tr>
+                                <td>{product.key}</td>
+                                <td>{product.name}</td>
+                                <td>{product.name}</td>
+                                <td>{product.category}</td>
+                                <td>{product.dimensions}</td>
+                                <td>{product.availability }</td>
+
+                             </tr>
+                           )
+                        })
+                     }
+                     </tbody>
+                     </table>
             <Link to="/admin/items/add">
              <CiCirclePlus className="text-[100px] absolute right-2 bottom-2 hover:text-red-900 "/> 
              </Link>  
