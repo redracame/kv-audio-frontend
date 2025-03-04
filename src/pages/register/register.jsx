74df03817cf0d8c01235d359634525df2a25cb67
redracame <linukaauchithya@gmail.com>
2025-03-04 17:22:41 +0530

    import css file✅

--- src/pages/register/register.jsx
+++ src/pages/register/register.jsx
@@ -1,6 +1,7 @@
+import "./register.css";
 export default function RegisterPage(){
       return(
-        <div className="bg-red-600 h-screen flex items-center justify-center">
+        <div className="bg-picture h-screen flex items-center justify-center">
             Register Page
         </div>
       )
