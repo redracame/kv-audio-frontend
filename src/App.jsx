f58bf99f110eca71c93de9c1371989f79c6ede0e
redracame <linukaauchithya@gmail.com>
2025-03-04 17:03:09 +0530

    create registerPage funtion.🤺

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,7 @@ function App(){
         <Route path="/login" element={<LoginPage/>}/>
         <Route path='/admin/*' element={<AdminPage/>}/>
         <Route path='/*' element={<HomePage/>}/>
+        
       
    </Routes>
     </BrowserRouter>
