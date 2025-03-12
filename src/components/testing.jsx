4b554934b07be9a8ac9473cd7e35e4220114e3e5
redracame <linukaauchithya@gmail.com>
2025-03-12 14:45:11 +0530

    try connect superbase ...firstly create meadia.js add it anon_key and superbase_url✅

--- src/components/testing.jsx
+++ src/components/testing.jsx
@@ -12,7 +12,7 @@ function UploadFile() {
       <div className="w-full flex flex-col justify-center items-center bg-gray-100  h-screen">
         <div className="bg-white p-6 rounded-lg shadow-lg">
         <input type="file" multiple onChange={(e)=>{setFile(e.target.files[0])}} className="mb-4"/>
-        <button className="w-[200px] h-[50px] bg-blue-500 text-white font-semibold py-2 rounded-lg hover:bg-blue-600 transition duration-150">
+        <button onClick={UploadFile} className="w-[200px] h-[50px] bg-blue-500 text-white font-semibold py-2 rounded-lg hover:bg-blue-600 transition duration-150">
           Upload
         </button>
       
---
+++ src/utils/mediaUpload.js
@@ -0,0 +1,3 @@
+
+const anon_key ="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imhmcmh3bHRnbW9lbmdmdWdocWVrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDE3NjI0NTEsImV4cCI6MjA1NzMzODQ1MX0.5uijNNEh5bECFKojJ5mtYm6px9eIfL9-MWwC3jpMeEY"
+const superbase_url = "https://hfrhwltgmoengfughqek.supabase.co"
