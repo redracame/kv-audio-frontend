5bbd6c28489bea9223c6b7ea119329f4d01ca319
redracame <linukaauchithya@gmail.com>
2025-03-12 14:29:52 +0530

    fixed issue in testing page✅

--- src/components/testing.jsx
+++ src/components/testing.jsx
@@ -7,12 +7,14 @@ const [file,setFile] = useState(null)
 
 
     return(
-      <div className="w-full h-screen">
-        <input type="file" multiple onChange={(e)=>{console.log(e.target.files[0])}}/>
-        <button className="w-[200px] h-[50px] text-white py-2 rounded-lg hover:bg-blue-600 transition duration-150">
-          upload
+      <div className="w-full flex flex-col justify-center items-center bg-gray-100  h-screen">
+        <div className="bg-white p-6 rounded-lg shadow-lg">
+        <input type="file" multiple onChange={(e)=>{setFile(e.target.files[0])}} className="mb-4"/>
+        <button className="w-[200px] h-[50px] bg-blue-500 text-white font-semibold py-2 rounded-lg hover:bg-blue-600 transition duration-150">
+          Upload
         </button>
       
+       </div>
        </div>
 )
 
