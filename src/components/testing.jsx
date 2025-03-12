ba70aff9a6e90b3ffa014b7f661e2c7c2d35e220
redracame <linukaauchithya@gmail.com>
2025-03-12 13:59:36 +0530

    edit testig file code to choose multiple choice:✅

--- src/components/testing.jsx
+++ src/components/testing.jsx
@@ -1,14 +1,17 @@
 import { useState } from "react";
 
 export default function Testing(){
-
+const [file,setFile] = useState(null)
 
 
 
 
     return(
       <div className="w-full h-screen">
-        Testing
+        <input type="file" multiple onChange={(e)=>{console.log(e.target.files[0])}}/>
+        <button className="w-[200px] h-[50px] text-white py-2 rounded-lg hover:bg-blue-600 transition duration-150">
+          upload
+        </button>
       
        </div>
 )
