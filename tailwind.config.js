12d3fe5f5122ed14b30200ddb4ee4c17272dbe7e
redracame <linukaauchithya@gmail.com>
2025-03-12 13:01:57 +0530

    add colors to web site '👍

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,13 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+      primary :"#FDFBEE",
+      secondary:"#57B4BA",
+      accent :"#205781"
+      }
+    },
   },
   plugins: [],
 }
